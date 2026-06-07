6aa41b0f17031719bfa1e17f3699925b30b5976b
Eduardo <edmalfizeo@proton.me>
2026-06-07 12:35:43 -0300

    feat: add Badge ui component

---
+++ src/components/ui/Badge.tsx
@@ -0,0 +1,70 @@
+/* eslint-disable react-refresh/only-export-components */
+import { cva, cx, type VariantProps } from "class-variance-authority";
+import Skeleton from "./Skeleton";
+import Text from "./Text";
+
+
+export const badgeVariants = cva(
+	"inline-flex items-center justify-center rounded-full ",
+	{
+		variants: {
+			variant: {
+				none: "",
+				primary: "bg-green-light",
+				secondary: "bg-pink-light",
+			},
+			size: {
+				sm: "px-2 py-0.5",
+			},
+		},
+		defaultVariants: { variant: "primary", size: "sm" },
+	},
+);
+
+export const badgeSkeletonVariants = cva("", {
+	variants: {
+		size: {
+			sm: "w-6 h-6"
+		}
+	},
+	defaultVariants: {
+		size: "sm"
+	}
+})
+
+export const badgeTextVariants = cva("", {
+	variants: {
+		variant: {
+			none: "",
+			primary: "text-green-dark",
+			secondary: "text-pink-dark",
+		},
+	},
+	defaultVariants: { variant: "primary" },
+});
+
+interface BadgeProps
+	extends React.ComponentProps<"div">,
+		VariantProps<typeof badgeVariants> {
+			loading?: boolean;
+		}
+
+export default function Badge({
+	variant,
+	size,
+	className,
+	children,
+	loading,
+	...props
+}: BadgeProps) {
+	if (loading) {
+		return <Skeleton rounded="full" className={cx(badgeVariants({variant: "none"}), badgeSkeletonVariants({size}), className)} />
+	}
+	return (
+		<div className={badgeVariants({ variant, size, className })} {...props}>
+			<Text variant="body-sm-bold" className={badgeTextVariants({ variant })}>
+				{children}
+			</Text>
+		</div>
+	);
+}
