a76425426e034fad68a73482b7eb3e013bb1f173
Eduardo <edmalfizeo@proton.me>
2026-06-07 12:32:49 -0300

    chore: add App root component

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import LayoutMain from "./pages/layout-main";
+import PageComponents from "./pages/page-components";
+import PageHome from "./pages/page-home";
+
+
+export default function App() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route element={<LayoutMain />}>
+					<Route index element={<PageHome />} />
+					<Route path="/components" element={<PageComponents />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
+}
